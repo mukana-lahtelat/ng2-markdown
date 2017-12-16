b1ab935301bbdb4152f12968c648240197794fc1
Evan Plaice <evanplaice@gmail.com>
2017-12-16 09:25:57 -0700

    Add angular2 module to src/ng2-markdown.module
    Can be used to extend ng2-markdown in the future

---
+++ index.js
@@ -0,0 +1,2 @@
+export { MarkdownComponent } from 'ng2-markdown/src/ng2-markdown';
+export { MarkdownModule } from 'ng2-markdown/src/ng2-markdown.module';
---
+++ src/ng2-markdown.module.js
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MarkdownComponent } from 'ng2-markdown';
+
+@NgModule({
+  declarations: [
+    MarkdownComponent
+  ],
+  exports: [
+    MarkdownComponent
+  ],
+})
+
+export class MarkdownModule { }
