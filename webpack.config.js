0f0ee44b8a95779411f510f3b3f35a499c7ab592
Evan Plaice <evanplaice@gmail.com>
2017-12-19 17:43:31 -0700

    Add webpack config required to build the dist bundle

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+
+  entry: './src/ng2-markdown.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [ 'es2015', 'angular2' ]
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  externals: {
+    "@angular/core": '@angular/core',
+    "@angular/http": '@angular/http'
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'ng2-markdown.js',
+    libraryTarget: 'umd',
+  }
+};
